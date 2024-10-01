94d7da84c4482927c2063caa87cded92470a9659
Daniiar Salaidinuulu <dsalaidinov@gmail.com>
2024-10-01 14:18:25 +0600

    Add remove duplicates from sorted array

---
+++ arrays-and-strings/remove-duplicates-from-sorted-array.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+
+        let mut i = 1;
+
+        for j in 1..nums.len() {
+            if nums[j] != nums[j-1] {
+                nums[i] = nums[j];
+                i += 1;
+            }
+
+        }
+
+        return i as i32;
+    }
+}
