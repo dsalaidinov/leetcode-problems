8d70732c9fe03bb4863530e7e871b60de25fc8ea
Daniiar Salaidinuulu <dsalaidinov@gmail.com>
2024-10-07 15:19:48 +0600

    Added 83. Remove Duplicates from Sorted List

---
+++ linked-lists/83. Remove Duplicates from Sorted List.rs
@@ -0,0 +1,36 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut current = head.as_mut();
+
+        while let Some(node) = current {
+            let mut next_node = node.next.take();
+
+            while let Some(next) = next_node.as_mut() {
+                if next.val == node.val {
+                    { next_node = next.next.take(); }
+                } else {
+                    node.next = next_node; break;
+                }
+            }
+            current = node.next.as_mut();
+        }
+
+        head
+    }
+}
