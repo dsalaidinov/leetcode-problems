f8e79e69147caaa615e6fe9b7006f2b9ab71fb04
Daniiar Salaidinuulu <dsalaidinov@gmail.com>
2024-10-08 15:47:57 +0600

    Added 643. Maximum Average Subarray I

---
+++ arrays-and-strings/643. Maximum Average Subarray I.rs
@@ -0,0 +1,21 @@
+use core::cmp::max;
+
+impl Solution {
+    pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+        let mut begin = 0;
+        let mut window_state = 0;
+        let mut result = i32::MIN;
+
+        for end in 0..nums.len() {
+            window_state += nums[end];
+
+            if end - begin + 1 == k as usize {
+                result = max(result, window_state);
+                window_state -= nums[begin];
+                begin += 1;
+            }
+        }
+
+        result as f64 / k as f64
+    }
+}
