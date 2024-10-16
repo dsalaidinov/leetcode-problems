27ae4e06f75302fa3dcf7231dcd4b0f8a274c44c
Daniiar Salaidinuulu <dsalaidinov@gmail.com>
2024-10-16 22:53:55 +0600

    Add 933. Number of Recent Calls.rs

---
+++ stacks-and-queues/933. Number of Recent Calls.rs
@@ -0,0 +1,39 @@
+use std::collections::VecDeque;
+
+struct RecentCounter {
+    queue: VecDeque<i32>,
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl RecentCounter {
+
+    fn new() -> Self {
+        RecentCounter {
+            queue: VecDeque::new(),
+        }
+    }
+    
+    fn ping(&mut self, t: i32) -> i32 {
+        &self.queue.push_back(t);
+
+        while let Some(&front) = &self.queue.front() {
+            if front < t - 3000 {
+                &self.queue.pop_front();
+            } else {
+                break;
+            }
+        }
+
+        self.queue.len() as i32
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * let obj = RecentCounter::new();
+ * let ret_1: i32 = obj.ping(t);
+ */
